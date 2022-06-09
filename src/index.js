4c0b4b7fe3746e804e1a65174fd9863cd9d3c32c
Nishat <nsalsabil25@gmail.com>
2022-06-09 11:11:30 -0700

    changed some constant names and deleted unused code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const state2 = {
+const state = {
   temperature: 40,
   soccer: 0,
 };
@@ -9,15 +9,15 @@ const state2 = {
 const changeTempColor = (event) => {
   const tempNumber = document.getElementById('temperature-number');
   let tempColor = 'black';
-  if (state2.temperature >= 80) {
+  if (state.temperature >= 80) {
     tempColor = 'red';
-  } else if (state2.temperature >= 70) {
+  } else if (state.temperature >= 70) {
     tempColor = 'orange';
-  } else if (state2.temperature >= 60) {
+  } else if (state.temperature >= 60) {
     tempColor = 'yellow';
-  } else if (state2.temperature >= 50) {
+  } else if (state.temperature >= 50) {
     tempColor = 'green';
-  } else if (state2.temperature <= 49) {
+  } else if (state.temperature <= 49) {
     tempColor = 'teal';
   }
   tempNumber.className = tempColor;
@@ -26,20 +26,20 @@ const changeTempColor = (event) => {
 
 //  function to decrease temp
 const decreaseTemp = (event) => {
-  state2.temperature -= 1;
+  state.temperature -= 1;
   const temperatureContainer = document.querySelector('#temperature-number');
-  temperatureContainer.textContent = `${state2.temperature}`;
+  temperatureContainer.textContent = `${state.temperature}`;
 };
 
 //  function to increase temp
 const increaseTemp = (event) => {
-  state2.temperature += 1;
+  state.temperature += 1;
   const temperatureContainer = document.querySelector('#temperature-number');
-  temperatureContainer.textContent = `${state2.temperature}`;
+  temperatureContainer.textContent = `${state.temperature}`;
 };
 
 // Register the event listener
-const registerEventHandlers2 = (event) => {
+const registerEventHandlers = (event) => {
   const upButton = document.querySelector('#up-button');
   const downButton = document.querySelector('#down-button');
   upButton.addEventListener('click', increaseTemp);
@@ -48,30 +48,7 @@ const registerEventHandlers2 = (event) => {
   downButton.addEventListener('click', changeTempColor);
 };
 
-document.addEventListener('DOMContentLoaded', registerEventHandlers2);
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
 
-// // We leave this here as a reminder for how to do stuff in Javascript
-// const state = {
-//   soccerCount: 0,
-// };
 
-// // Add a soccer ball
-// const addSoccer = (event) => {
-//   const newSoccer = document.createElement('span');
-//   const soccerContainer = document.querySelector('#soccerContainer');
-//   newSoccer.textContent = '⚽';
-//   soccerContainer.appendChild(newSoccer);
 
-//   // Soccer Count Behavior
-//   state.soccerCount += 1;
-//   const soccerCountContainer = document.querySelector('#soccerCount');
-//   soccerCountContainer.textContent = `Soccer Count: ${state.soccerCount}`;
-// };
-
-// // Register the event listener
-// const registerEventHandlers = (event) => {
-//   const soccerButton = document.querySelector('#addSoccerButton');
-//   soccerButton.addEventListener('click', addSoccer);
-// };
-
-// document.addEventListener('DOMContentLoaded', registerEventHandlers);
