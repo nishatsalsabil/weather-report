f1af86e29f5d84e04e335898432f391575730018
Sophia Wagner <sophiewagnerla@gmail.com>
2022-06-09 11:02:37 -0700

    Combine event listeners, add color change functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const state2 = {
-  temperature: 0,
+  temperature: 40,
   soccer: 0,
 };
 
+// function to change temp color
 const changeTempColor = (event) => {
   const tempNumber = document.getElementById('temperature-number');
   let tempColor = 'black';
@@ -20,17 +21,17 @@ const changeTempColor = (event) => {
     tempColor = 'teal';
   }
   tempNumber.className = tempColor;
-};
-const registerEventHandlers4 = (event) => {
-  const upButton = document.querySelector('#up-button');
-  upButton.addEventListener('click', changeTempColor);
-};
-const registerEventHandlers5 = (event) => {
-  const downButton = document.querySelector('#down-button');
-  downButton.addEventListener('click', changeTempColor);
+  // tempNumber.setAttribute(color, tempColor); can't get this to work, will use class instead
 };
 
-// Add function to increaseTemp
+//  function to decrease temp
+const decreaseTemp = (event) => {
+  state2.temperature -= 1;
+  const temperatureContainer = document.querySelector('#temperature-number');
+  temperatureContainer.textContent = `${state2.temperature}`;
+};
+
+//  function to increase temp
 const increaseTemp = (event) => {
   state2.temperature += 1;
   const temperatureContainer = document.querySelector('#temperature-number');
@@ -40,48 +41,37 @@ const increaseTemp = (event) => {
 // Register the event listener
 const registerEventHandlers2 = (event) => {
   const upButton = document.querySelector('#up-button');
+  const downButton = document.querySelector('#down-button');
   upButton.addEventListener('click', increaseTemp);
+  downButton.addEventListener('click', decreaseTemp);
+  upButton.addEventListener('click', changeTempColor);
+  downButton.addEventListener('click', changeTempColor);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers2);
 
-// Add function to decrease temp
-const decreaseTemp = (event) => {
-  state2.temperature -= 1;
-  const temperatureContainer = document.querySelector('#temperature-number');
-  temperatureContainer.textContent = `${state2.temperature}`;
-};
+// // We leave this here as a reminder for how to do stuff in Javascript
+// const state = {
+//   soccerCount: 0,
+// };
 
-// Register the event listener
-const registerEventHandlers3 = (event) => {
-  const upButton = document.querySelector('#down-button');
-  upButton.addEventListener('click', decreaseTemp);
-};
+// // Add a soccer ball
+// const addSoccer = (event) => {
+//   const newSoccer = document.createElement('span');
+//   const soccerContainer = document.querySelector('#soccerContainer');
+//   newSoccer.textContent = '⚽';
+//   soccerContainer.appendChild(newSoccer);
 
-document.addEventListener('DOMContentLoaded', registerEventHandlers3);
+//   // Soccer Count Behavior
+//   state.soccerCount += 1;
+//   const soccerCountContainer = document.querySelector('#soccerCount');
+//   soccerCountContainer.textContent = `Soccer Count: ${state.soccerCount}`;
+// };
 
-// We leave this here as a reminder for how to do stuff in Javascript
-const state = {
-  soccerCount: 0,
-};
+// // Register the event listener
+// const registerEventHandlers = (event) => {
+//   const soccerButton = document.querySelector('#addSoccerButton');
+//   soccerButton.addEventListener('click', addSoccer);
+// };
 
-// Add a soccer ball
-const addSoccer = (event) => {
-  const newSoccer = document.createElement('span');
-  const soccerContainer = document.querySelector('#soccerContainer');
-  newSoccer.textContent = '⚽';
-  soccerContainer.appendChild(newSoccer);
-
-  // Soccer Count Behavior
-  state.soccerCount += 1;
-  const soccerCountContainer = document.querySelector('#soccerCount');
-  soccerCountContainer.textContent = `Soccer Count: ${state.soccerCount}`;
-};
-
-// Register the event listener
-const registerEventHandlers = (event) => {
-  const soccerButton = document.querySelector('#addSoccerButton');
-  soccerButton.addEventListener('click', addSoccer);
-};
-
-document.addEventListener('DOMContentLoaded', registerEventHandlers);
+// document.addEventListener('DOMContentLoaded', registerEventHandlers);
